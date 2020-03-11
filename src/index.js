eb4280f26b6a6638f8c191b94a5b486a5a195264
马晓翔 <maxx@zoenet.cn>
2020-03-11 16:11:38 +0800

    feat(初版打包):初版测试

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-function replaceShell(name) {
-  console.log('hello:' + name);
+const weblog = require('webpack-log');
+const log = weblog({name: 'zoe-skeleton'});
+const {writeSkeleton} = require('./util/index');
+const Server = require('./server');
+
+function SkeletonPlugin(options = {}) {
+  this.options = options;
+  this.server = null;
+  log.info(options);
 }
 
-module.exports = replaceShell;
+SkeletonPlugin.prototype.createServer = function () {
+  const server = this.server = new Server(this.options);
+  server.listen().catch(err => log.warn(err));
+};
+
+SkeletonPlugin.prototype.apply = function (compiler) {
+  this.createServer();
+};
+
+module.exports = SkeletonPlugin;
