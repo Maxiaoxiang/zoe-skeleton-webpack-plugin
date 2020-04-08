b381cfed6818e3f3e545bd0a9da0685d6bfdacd4
马晓翔 <maxx@zoenet.cn>
2020-04-08 10:51:55 +0800

    feat(package-lock): 更新包

--- src/util/index.js
+++ src/util/index.js
@@ -115,7 +115,7 @@ let isExist = function (fullpath) {
 };
 
 
-//写入views模板文件引入（新版title-config路口开放后该功能未来可能移除）
+//写入views模板文件引入（新版title-config路口开放后该功能未来可能移除）.
 const writeView = async (staticDir, viewsDir, url) => {
     let urlStr = url.split('?')[0];
     return new Promise((async (resolve) => {
